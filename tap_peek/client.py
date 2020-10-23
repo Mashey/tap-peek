fc9ffb47e508d5fec1018f82bcd9b59c06581071
Michael Cooper <macoop2363@gmail.com>
2020-10-23 11:20:39 -0600

    Move client methods to alphabetical order and add page parameter to transactions

--- tap_peek/client.py
+++ tap_peek/client.py
@@ -8,15 +8,6 @@ class PeekClient:
         self._client = requests.Session()
         self._client.headers.update({'Authorization': api_key})
 
-    def fetch_transactions(self, partner_id, start_date="2020-08-01", end_date="2020-08-25"):
-        url    = f"{self.BASE_URL}/reporting/api/reporting/transaction_records"
-        params = {
-            "fiql_query": f"partner_id=={partner_id};record_type=in=(purchase,reservation);purchase_type=in=(activity,addon,security_deposit,additional_charge);purchase_date=ge={start_date};purchase_date=le={end_date};currency==usd",
-            "page_size": "50",
-            "page": "use meta.next_page"
-        }
-        return self._client.get(url, params=params).json()
-
     def fetch_core_activities(self, partner_id):
         url    = f"{self.BASE_URL}/pro/core/accounts"
         params = {
@@ -29,10 +20,19 @@ class PeekClient:
         url = f"{self.BASE_URL}/once-pro/api/activities/partner/{partner_id}"
         return self._client.get(url).json()
 
-    def fetch_timeslots(self, start_date="2020-08-25", end_date="2020-10-15"):
+    def fetch_timeslots(self, start_date="2019-08-25", end_date="2020-10-15"):
         url    = f"{self.BASE_URL}/once-pro/api/timeslots"
         params = {
             "start_date": start_date,
             "end_date": end_date
+        }
+        return self._client.get(url, params=params).json()
+
+    def fetch_transactions(self, partner_id, start_date="2020-08-01", end_date="2020-08-25", page=1):
+        url    = f"{self.BASE_URL}/reporting/api/reporting/transaction_records"
+        params = {
+            "fiql_query": f"partner_id=={partner_id};record_type=in=(purchase,reservation);purchase_type=in=(activity,addon,security_deposit,additional_charge);purchase_date=ge={start_date};purchase_date=le={end_date};currency==usd",
+            "page_size": "50",
+            "page": page
         }
         return self._client.get(url, params=params).json()
